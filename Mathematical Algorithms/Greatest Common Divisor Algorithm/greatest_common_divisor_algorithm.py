50ff8e8d8b27dc2a6acbfe3e9f78ad0ae86f1b14
Imam Kilicaslan <imamkilicaslan@gmail.com>
2022-10-25 21:35:16 +0300

    .

---
+++ Mathematical Algorithms/Greatest Common Divisor Algorithm/greatest_common_divisor_algorithm.py
@@ -0,0 +1,6 @@
+number1 = int(input("input a number for gcd calculation"))
+number2 = int(input("input a more one number for gcd calculation"))
+
+bigest_number = number1 if number1 > number2 else number2
+print(bigest_number)
+# done ##########
